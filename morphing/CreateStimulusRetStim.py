421424f8f25ed9cf3d86ce9d5f03f52fcecacef0
Sonboth <linhardtdavid@gmail.com>
2022-08-18 14:05:59 +0200

    bug fix for not immediate

--- morphing/CreateStimulusRetStim.py
+++ morphing/CreateStimulusRetStim.py
@@ -26,21 +26,19 @@ join = os.path.join
 
 ###### EDIT ##########
 ## BCBL ##
-RP = "/Users/glerma/toolboxes/BCBLViennaSoft"
-triggerKey = "bcbl"
+# RP = "/Users/glerma/toolboxes/BCBLViennaSoft"
+# triggerKey = "bcbl"
 
 ## VIENNA ##
-# RP = "/local/dlinhardt/develop/BCBLViennaSoft"
-# triggerKey = "6"  # "bcbl"
+RP = "/local/dlinhardt/develop/BCBLViennaSoft"
+triggerKey = "6"  # "bcbl"
 
 ###### END EDIT ##########
 
 
-
-
 localpath = join(RP, "local")
 stimSize  = 1024
-maxEcc    = 8 # 9 Vienna, 8 BCBL until changes
+maxEcc    = 8  # 9 Vienna, 8 BCBL until changes
 overlap   = 1 / 3
 
 duration = 300
@@ -49,42 +47,42 @@ forceBarWidth = 2
 
 # trs = [0.8, 1]
 # flickerFrequency = [2.5, 2]
-trs_flickerFreqs = [(1,2)] # [(0.8, 2.5), (1, 2)]
+trs_flickerFreqs = [(1, 2)]  # [(0.8, 2.5), (1, 2)]
 
 # langs = ["ES","AT"]
 # imnames = ["CB", "PW", "FF", "RW", "PW10", "PW20", "FF10", "FF20", "RW10", "RW20"]
 langs = ["ES"]
 imnames = ["CB", "PW", "RW"]
 
-for (tr,flickerFrequency) in trs_flickerFreqs:
+for (tr, flickerFrequency) in trs_flickerFreqs:
     for lang in langs:
         for imname in imnames:
             imfilename = f"{lang}_{imname}_{stimSize}x{stimSize}x100.mat"
             loadImages = join(RP, "morphing", "DATA", "retWordsMagno", imfilename)
             if imname == "CB":
                 stim = barStimulus(
-                                    stimSize=stimSize,
-                                    maxEcc=maxEcc,
-                                    overlap=overlap,
-                                    TR=tr,
-                                    stim_duration=duration,
-                                    blank_duration=blank_duration,
-                                    flickerFrequency=flickerFrequency
-                                )
+                    stimSize=stimSize,
+                    maxEcc=maxEcc,
+                    overlap=overlap,
+                    TR=tr,
+                    stim_duration=duration,
+                    blank_duration=blank_duration,
+                    flickerFrequency=flickerFrequency,
+                    forceBarWidth=forceBarWidth,
+                )
             else:
                 stim = barStimulus(
-                                    stimSize=stimSize,
-                                    maxEcc=maxEcc,
-                                    overlap=overlap,
-                                    TR=tr,
-                                    stim_duration=duration,
-                                    blank_duration=blank_duration,
-                                    flickerFrequency=flickerFrequency,
-                                    loadImages=loadImages
-                                )
+                    stimSize=stimSize,
+                    maxEcc=maxEcc,
+                    overlap=overlap,
+                    TR=tr,
+                    stim_duration=duration,
+                    blank_duration=blank_duration,
+                    flickerFrequency=flickerFrequency,
+                    loadImages=loadImages,
+                    forceBarWidth=forceBarWidth,
+                )
             oName = f"{lang}_{imname}_tr-{tr}_duration-{duration}sec" \
                     f"_size-{stimSize}.mat"
             oPath = join(localpath, oName)
             stim.saveMrVistaStimulus(oPath, triggerKey=triggerKey)
-            
-            
