d8759d553d76418affb54e667940b710f20b1316
Garikoitz <garikoitz@gmail.com>
2024-12-20 10:42:17 +0100

    for tiger ES exp

--- morphing/CreateStimulusRetStim.py
+++ morphing/CreateStimulusRetStim.py
@@ -65,7 +65,7 @@ trs_flickerFreqs = [(2, 8)]  # This supposed to mimic Stanford's CNI and Tel Avi
 
 # langs = ["ES","AT"]
 # imnames = ["CB", "PW", "FF", "RW", "PW10", "PW20", "FF10", "FF20", "RW10", "RW20"]
-langs = ["IT"] # ,"ES", "AT", "FR"]  # ["ES", "IT", "AT", "FR"]  # , "AT"
+langs = ["ES", "AT", "FR"]  # ["ES", "IT", "AT", "FR"]  # , "AT"
 # imnames = ["CB", "RW"]
 
 # Create one imname per every step in the morphing
